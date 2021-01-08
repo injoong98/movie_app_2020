00639f9e6f5637d6b7377f61728eec1bfdcb2a82
injoong98 <68066289+injoong98@users.noreply.github.com>
2021-01-08 19:40:17 +0900

    push

---
+++ src/routes/Detail.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+class Detail extends React.Component{
+    
+    componentDidMount() {
+        const {location, history} = this.props;
+        if(location.state ===undefined){
+            history.push("/");
+        }
+    }
+    
+    
+    render() {
+        const {location} = this.props;
+        return(
+            <div>{location.state.title}</div>
+        )
+    }
+}
+export default Detail;
